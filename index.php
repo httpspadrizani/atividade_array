4c0f7f1cfb6570e1435067a760e14eb98f2eb9b4
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-03-08 12:09:45 -0300

    ATIVIDADE

---
+++ index.php
@@ -0,0 +1,43 @@
+    <?php
+        $pessoas1 = array();
+        $pessoas2 = array();
+        $pessoas3 = array();
+        $pessoas4 = array();
+            
+            $pessoas1 [0][0][0][0][0][0] = "Maria";
+            $pessoas1 [0][0][0][0][0][1] = "16 anos";
+            $pessoas1 [0][0][0][0][0][2] = "peixes";
+            $pessoas1 [0][0][0][0][0][3] = "Alice no País das Maravilhas";
+            $pessoas1 [0][0][0][0][0][4] = "Os originais";
+            $pessoas1 [0][0][0][0][0][5] = "A guardia de Muirakuitãs";
+
+            $pessoas2 [0][0][0][0][0][0] = "Ana";
+            $pessoas2 [0][0][0][0][1][1] = "16 anos";
+            $pessoas2 [0][0][0][0][2][2] = "peixes";
+            $pessoas2 [0][0][0][0][3][3] = "Invocação do mal";
+            $pessoas2 [0][0][0][0][4][4] = "Diario de um vampiro";
+            $pessoas2 [0][0][0][0][5][5] = "Acotar";
+
+            $pessoas3 [0][0][0][0][0][0] = "João";
+            $pessoas3 [0][0][0][1][1][1] = "15 anos";
+            $pessoas3 [0][0][0][2][2][2] = "virgem";
+            $pessoas3 [0][0][0][3][3][3] = "Batman o cavaleiro das trevas";
+            $pessoas3 [0][0][0][4][4][4] = "Teen wolf";
+            $pessoas3 [0][0][0][5][5][5] = "O Iluminado";
+
+            $pessoas4 [0][0][0][0][0][0] = "Matheus";
+            $pessoas4 [0][0][1][1][1][1] = "16 anos";
+            $pessoas4 [0][0][2][2][2][2] = "cancêr";
+            $pessoas4 [0][0][3][3][3][3] = "Onde está segunda ?";
+            $pessoas4 [0][0][4][4][4][4] = "Pokémon";
+            $pessoas4 [0][0][5][5][5][5] = "Coraline";
+
+           echo " A " . $pessoas1[0][0][0][0][0][0] . " e tem " . $pessoas1[0][0][0][0][0][1] . " o signo dela é " . $pessoas1[0][0][0][0][0][2] . " o filme favorito dela é " . $pessoas1[0][0][0][0][0][3] . " a serie favorita dela é " . $pessoas1[0][0][0][0][0][4] . " o livro favorito dela é " . $pessoas1[0][0][0][0][0][5];
+echo("</br>");
+           echo " A " . $pessoas2[0][0][0][0][0][0] . " e tem " . $pessoas2[0][0][0][0][1][1] . " o signo dela é " . $pessoas2[0][0][0][0][2][2] . " o filme favorito dela é " . $pessoas2[0][0][0][0][3][3] . " a serie favorita dela é " . $pessoas2[0][0][0][0][4][4] . " o livro favorito dela é " . $pessoas2[0][0][0][0][5][5];
+echo("</br>");
+           echo " O " . $pessoas3[0][0][0][0][0][0] . " e tem " . $pessoas3[0][0][0][1][1][1] . " o signo dela é " . $pessoas3[0][0][0][2][2][2] . " o filme favorito dela é " . $pessoas3[0][0][0][3][3][3] . " a serie favorita dela é " . $pessoas3[0][0][0][4][4][4] . " o livro favorito dela é " . $pessoas3[0][0][0][5][5][5];
+echo("</br>");
+           echo " O " . $pessoas4[0][0][0][0][0][0] . " e tem " . $pessoas4[0][0][1][1][1][1] . " o signo dela é " . $pessoas4[0][0][2][2][2][2] . " o filme favorito dela é " . $pessoas4[0][0][3][3][3][3] . " a serie favorita dela é " . $pessoas4[0][0][4][4][4][4] . " o livro favorito dela é " . $pessoas4[0][0][5][5][5][5];
+echo("</br>");
+?>
